f4aa318e5a4be0e256224f0f87cc61438ed53927
Mahesh Patil <imaheshpatil2003@gmail.com>
2026-08-10 01:16:14 +0530

    Merge Layout

--- src/components/ToolWorkspace.jsx
+++ src/components/ToolWorkspace.jsx
@@ -1,9 +1,38 @@
 import Navbar from './Navbar';
 
-export default function ToolWorkspace() {
+export default function ToolWorkspace({
+  title,
+  children,
+  sidebar
+}) {
   return (
-    <div className="bg-red-500 text-white text-5xl p-20">
-      TEST WORKSPACE
+    <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black text-white">
+
+      <Navbar />
+
+      <div className="flex min-h-[calc(100vh-72px)]">
+
+        {/* MAIN WORKSPACE */}
+        <main className="flex-1 min-w-0 p-8">
+          {children}
+        </main>
+
+        {/* RIGHT SIDEBAR */}
+        <aside
+          className="
+            w-[340px]
+            flex-shrink-0
+            border-l
+            border-white/10
+            bg-black/20
+            p-6
+          "
+        >
+          {sidebar}
+        </aside>
+
+      </div>
+
     </div>
   );
 }
